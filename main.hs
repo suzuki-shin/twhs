9e327b5110a85d1c4c487bd9ba279cac552218d5
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-05-02 18:21:57 +0900

    rm commented code

--- main.hs
+++ main.hs
@@ -10,21 +10,8 @@ import Web.Twitter.Conduit
 import System.Environment
 
 import Control.Lens
--- import Web.Authenticate.OAuth (OAuth(..), Credential(..))
--- import qualified Web.Authenticate.OAuth as OA
--- import Network.HTTP.Conduit
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
--- import qualified Data.Text as T
--- import qualified Data.Text.IO as T
--- import qualified Data.ByteString.Char8 as B8
--- import Data.Default
--- import Control.Monad.Logger
--- import Control.Monad.Trans.Control
--- import Control.Monad.Trans.Resource
--- import Control.Monad.IO.Class
--- import System.IO (hFlush, stdout)
-
 
 import Common
 
@@ -34,7 +21,6 @@ main = do
     print args
     if null args
         then timeline
---         then putStr "hoge"
         else post
 
 post :: IO ()
