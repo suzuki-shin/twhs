90294adf602253f1d627925d0295dcc809aa41ba
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-05-19 20:50:27 +0900

    misc

---
+++ TwhsConfig.hs
@@ -0,0 +1,6 @@
+module TwhsConfig (oauthConsumerKey, oauthConsumerSecret) where
+
+oauthConsumerKey :: String
+oauthConsumerKey = "UoNgjgh43ukUCkKBtCAFNYg96"
+oauthConsumerSecret :: String
+oauthConsumerSecret = "IEuzxBR7ykDtB9iEy7UkipQyzKmtLksd5gMSTzmsYXOgQsl5XD"
