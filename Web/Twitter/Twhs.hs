b55334480a5eba864d32cd28d26652f34ef42a45
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2014-05-24 22:23:28 +0900

    Update Twhs.hs
    remove comment outed code.

--- Web/Twitter/Twhs.hs
+++ Web/Twitter/Twhs.hs
@@ -2,13 +2,3 @@ module Web.Twitter.Twhs (module X) where
 import Web.Twitter.Twhs.Common as X
 import Web.Twitter.Twhs.Command as X
 import Web.Twitter.Twhs.Option as X
-
--- module Web.Twitter.Twhs (
---     module Web.Twitter.Twhs.Command
---   , module Web.Twitter.Twhs.Common
---   , module Web.Twitter.Twhs.Option
---   ) where
-
--- import Web.Twitter.Twhs.Common
--- import Web.Twitter.Twhs.Command
--- import Web.Twitter.Twhs.Option
