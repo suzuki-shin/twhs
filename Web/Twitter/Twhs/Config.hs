66418ecef86f531e818e8327d1176fd9ec87d41d
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2014-11-25 15:45:59 +0900

    Update Config.hs

--- Web/Twitter/Twhs/Config.hs
+++ Web/Twitter/Twhs/Config.hs
@@ -1,6 +1,6 @@
 module Web.Twitter.Twhs.Config (oauthConsumerKey, oauthConsumerSecret) where
 
 oauthConsumerKey :: String
-oauthConsumerKey = "UoNgjgh43ukUCkKBtCAFNYg96"
+oauthConsumerKey = "YOUR CONSUMER KEY"
 oauthConsumerSecret :: String
-oauthConsumerSecret = "IEuzxBR7ykDtB9iEy7UkipQyzKmtLksd5gMSTzmsYXOgQsl5XD"
+oauthConsumerSecret = "YOUR CONSUMER SECRET"
