4e8932c0d864af097049290c3cdef6fc4d746230
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-11-29 16:29:04 +0900

    streaming API作ってる途中

--- Web/Twitter/Twhs/Command.hs
+++ Web/Twitter/Twhs/Command.hs
@@ -32,6 +32,25 @@ import Network.HTTP.Conduit
 import Web.Twitter.Twhs.Common
 import Web.Twitter.Twhs.Option
 
+
+
+
+import Web.Twitter.Types.Lens
+import Control.Lens
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import System.Process
+import Control.Monad
+import System.Directory
+import System.FilePath
+import Network.HTTP.Conduit as HTTP
+import qualified Data.Conduit as C
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
+
+
+
+
 type TweetID = Integer
 type UserScreenName = String
 
@@ -124,3 +143,69 @@ colorStr fgi fg bgi bg str = do
   setSGR [SetColor Foreground fgi fg, SetColor Background bgi bg]
   putStr str
   setSGR []
+
+
+
+
+streaming = do
+    twInfo <- getTWInfoFromEnv
+    withManager $ \mgr -> do
+        src <- stream twInfo mgr userstream
+        src C.$$+- CL.mapM_ (^! act (liftIO . printTL))
+
+printTL :: StreamingAPI -> IO ()
+printTL (SStatus s) = T.putStrLn . showStatus $ s
+printTL (SRetweetedStatus s) = T.putStrLn $ T.concat [ s ^. user . userScreenName
+                                                     , ": RT @"
+                                                     , showStatus (s ^. rsRetweetedStatus)
+                                                     ]
+printTL (SEvent event)
+    | (event^.evEvent) == "favorite" || (event^.evEvent) == "unfavorite",
+      Just (ETStatus st) <- (event ^. evTargetObject) = do
+          let (fromUser, fromIcon) = evUserInfo (event^.evSource)
+              (toUser, _toIcon) = evUserInfo (event^.evTarget)
+              evUserInfo (ETUser u) = (u ^. userScreenName, u ^. userProfileImageURL)
+              evUserInfo _ = ("", Nothing)
+              header = T.concat [ event ^. evEvent, "[", fromUser, " -> ", toUser, "]"]
+          T.putStrLn $ T.concat [ header, " :: ", showStatus st ]
+          icon <- case fromIcon of
+              Just iconUrl -> Just <$> fetchIcon (T.unpack fromUser) (T.unpack iconUrl)
+              Nothing -> return Nothing
+          notifySend header (showStatus st) icon
+printTL s = print s
+
+showStatus :: AsStatus s => s -> T.Text
+showStatus s = T.concat [ s ^. user . userScreenName
+                        , ":"
+                        , s ^. text
+                        ]
+
+notifySend :: T.Text -> T.Text -> Maybe FilePath -> IO ()
+notifySend header content icon = do
+    let ic = maybe [] (\i -> ["-i", i]) icon
+    void $ rawSystem "notify-send" $ [T.unpack header, T.unpack content] ++ ic
+
+fetchIcon :: String -- ^ screen name
+          -> String -- ^ icon url
+          -> IO String
+fetchIcon sn url = do
+    ipath <- iconPath
+    let fname = ipath </> sn ++ "__" ++ takeFileName url
+    exists <- doesFileExist fname
+    unless exists $ withManager $ \mgr -> do
+        req <- liftIO $ parseUrl url
+        body <- http req mgr
+        HTTP.responseBody body C.$$+- CB.sinkFile fname
+    return fname
+
+iconPath :: IO FilePath
+iconPath = (</> "icons") <$> confdir >>= ensureDirectoryExist
+
+ensureDirectoryExist :: FilePath -> IO FilePath
+ensureDirectoryExist dir = do
+    createDirectoryIfMissing True dir
+    return dir
+
+confdir :: IO FilePath
+confdir = fmap (</> ".twitter-conduit") getHomeDirectory >>= ensureDirectoryExist
+
